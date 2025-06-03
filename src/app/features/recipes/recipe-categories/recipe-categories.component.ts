1b4c1c69da564a64bf1b3252168fa810d798f2fd
MattheoC-code <mattheo.coppin@ecoles-epsi.net>
2025-06-03 11:15:01 +0200

    feats : creation de  features/recipes/recipe-category features/recipes/recipe-categories features/recipes/recipe-card

---
+++ src/app/features/recipes/recipe-categories/recipe-categories.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-recipe-categories',
+  imports: [],
+  templateUrl: './recipe-categories.component.html',
+  styleUrl: './recipe-categories.component.scss'
+})
+export class RecipeCategoriesComponent {
+
+}
---
+++ src/app/features/recipes/recipe-category/recipe-category.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-recipe-category',
+  imports: [],
+  templateUrl: './recipe-category.component.html',
+  styleUrl: './recipe-category.component.scss'
+})
+export class RecipeCategoryComponent {
+
+}
